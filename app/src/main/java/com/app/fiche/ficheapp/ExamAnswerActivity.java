10fe363d5fb756a950fb0b9b82c03d054301e13e
HoxleyGit <hoxley246@gmail.com>
2018-03-22 18:51:49 +0100

    new acitivies, exam answer, exam question, exam result

---
+++ app/src/main/java/com/app/fiche/ficheapp/ExamAnswerActivity.java
@@ -0,0 +1,13 @@
+package com.app.fiche.ficheapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamAnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_answer);
+    }
+}
---
+++ app/src/main/java/com/app/fiche/ficheapp/ExamQuestionActivity.java
@@ -0,0 +1,13 @@
+package com.app.fiche.ficheapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamQuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam_question);
+    }
+}
