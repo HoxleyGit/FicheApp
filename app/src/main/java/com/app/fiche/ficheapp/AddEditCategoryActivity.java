b0c1c4953e9d163179ff658ce1f1ebf6f18b907d
lukHoracy <lukaspluciennik@gmail.com>
2018-03-25 22:53:05 +0200

    Buttons activities - update

--- app/src/main/java/com/app/fiche/ficheapp/AddEditCategoryActivity.java
+++ app/src/main/java/com/app/fiche/ficheapp/AddEditCategoryActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddEditCategoryActivity extends AppCompatActivity {
 
@@ -25,5 +27,30 @@ public class AddEditCategoryActivity extends AppCompatActivity {
         Button backToAllCategoriesButton = findViewById(R.id.button_back_to_all_categories);
         backToAllCategoriesButton.setOnClickListener(backToAllCategories);
 
+        Button addEditCategoryButton = findViewById(R.id.confirm_add_edit_category);
+        addEditCategoryButton.setOnClickListener(addEditCategoryListiner);
+
+
     }
+
+
+    private View.OnClickListener addEditCategoryListiner = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText categoryName = findViewById(R.id.nameofcategory1);
+            String categoryNameString = categoryName.getText().toString();
+
+            if (categoryNameString.equals(getResources().getString(R.string.add_name_category))) {
+                Toast toast = Toast.makeText(getBaseContext(), getResources().getString(R.string.no_category), Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+                Intent addEditCategoryIntent = new Intent(getBaseContext(), AllLessonsActivity.class);
+                startActivity(addEditCategoryIntent);
+
+                finish();
+
+            }
+        }
+    };
+
 }
--- app/src/main/java/com/app/fiche/ficheapp/AllLessonsActivity.java
+++ app/src/main/java/com/app/fiche/ficheapp/AllLessonsActivity.java
@@ -1,7 +1,10 @@
 package com.app.fiche.ficheapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AllLessonsActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class AllLessonsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_lessons);
+
+        Button backToAddEditCategoryButton = findViewById(R.id.button_to_categories);
+        backToAddEditCategoryButton.setOnClickListener(backToAddEditCategoryListiner);
     }
+
+    private View.OnClickListener backToAddEditCategoryListiner = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent backToAddEditCategoryIntent = new Intent(getBaseContext(),AddEditCategoryActivity.class);
+            startActivity(backToAddEditCategoryIntent);
+            finish();
+        }
+    };
 }
